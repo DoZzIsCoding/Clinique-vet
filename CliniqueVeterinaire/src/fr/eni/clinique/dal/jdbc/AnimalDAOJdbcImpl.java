3542fa4b3ab10ac7f7fe4d2c43f42e6da8e74412
dguiton2018 <dguiton2018@41-UC44-04.ad.campus-eni.fr>
2018-04-23 17:05:33 +0200

    methode selectbyid et insert (quasiment) AnimalDAO

--- CliniqueVeterinaire/src/fr/eni/clinique/dal/jdbc/AnimalDAOJdbcImpl.java
+++ CliniqueVeterinaire/src/fr/eni/clinique/dal/jdbc/AnimalDAOJdbcImpl.java
@@ -1,6 +1,7 @@
 package fr.eni.clinique.dal.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,12 +14,30 @@ import fr.eni.clinique.dal.DalException;
 
 public class AnimalDAOJdbcImpl implements AnimalDAO {
 
-	private static final String SELECT_ALL = "SELECT codeAnimal, nomAnimal, sexe, couleur, race, espece, codeClient, tatouage, antecedents, archive FROM animaux;";
+	private static final String SELECT_ALL = "SELECT codeAnimal, nomAnimal, sexe, couleur, race, espece, codeClient, tatouage, antecedents, archive FROM animaux";
+	
+	private static final String SELECT_BY_ID = SELECT_ALL + " WHERE codeAnimal =?";	
+	
+	private static final String INSERT = "INSERT INTO ANIMAL(nomAnimal, sexe, couleur, race, espece, codeClient, tatouage, antecedents, archive ) "
+										+ "VALUES(?,?,?,?,?,?,?,?,?)";;
+
 	
 	
 	@Override
 	public Animal selectionnerUn(int id) {
-		// TODO Auto-generated method stub
+		Animal animal = null;
+		
+		try (Connection cnx = ConnectionDAO.getConnection()){
+			PreparedStatement pstmt = cnx.prepareStatement(SELECT_BY_ID);
+			pstmt.setInt(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			if(rs.next()){
+				animal = this.itemBuilder(rs);
+			}
+			return animal;
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
 		return null;
 	}
 
@@ -39,18 +58,18 @@ public class AnimalDAOJdbcImpl implements AnimalDAO {
 		return animaux;
 	}
 
-	private Animal itemBuilder(ResultSet rs) throws SQLException {
-		Animal animal = new Animal(rs.getInt("CodeAnimal"), rs.getString("NomAnimal"),
-									rs.getString("Sexe"), rs.getString("Couleur"), rs.getString("Race"),
-									rs.getString("Espece"), rs.getInt("CodeClient"), rs.getString("Tatouage"), 
-									rs.getString("Antecedents"));
-		
-		return animal;
-	}
 
 	@Override
 	public void ajouter(Animal animal) {
-		// TODO Auto-generated method stub
+		/*if(animal == null){
+			throw NullPointerException();
+		}*/
+		
+		try (Connection cnx = ConnectionDAO.getConnection()){
+			PreparedStatement pstmt = cnx.prepareStatement(INSERT,Statement.RETURN_GENERATED_KEYS);
+			// reste le resultset, et l'inssertion à faire demain :)
+		
+		}
 		
 	}
 
@@ -66,6 +85,14 @@ public class AnimalDAOJdbcImpl implements AnimalDAO {
 		return false;
 	}
 
+	private Animal itemBuilder(ResultSet rs) throws SQLException {
+		Animal animal = new Animal(rs.getInt("CodeAnimal"), rs.getString("NomAnimal"),
+				rs.getString("Sexe"), rs.getString("Couleur"), rs.getString("Race"),
+				rs.getString("Espece"), rs.getInt("CodeClient"), rs.getString("Tatouage"), 
+				rs.getString("Antecedents"));
+		
+		return animal;
+	}
 	
 	
 }
