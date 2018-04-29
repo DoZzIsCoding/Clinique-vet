74519863e678fea83aa81ee86a868830c192b9ca
doris974 <delphine.guiton2018@campus-eni.fr>
2018-04-29 15:09:45 +0200

    frameAnimaux complète sans mise en forme

--- CliniqueVeterinaire/src/fr/eni/clinique/ihm/FrameAnimaux.java
+++ CliniqueVeterinaire/src/fr/eni/clinique/ihm/FrameAnimaux.java
@@ -2,22 +2,28 @@ package fr.eni.clinique.ihm;
 
 import java.awt.ComponentOrientation;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 public class FrameAnimaux extends JFrame {
 
 	public FrameAnimaux() {
 		setTitle("Animaux");
-		setBounds(100, 100, 400, 300);
+		setBounds(100, 100, 500, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		setContentPane(getMainPanel());
@@ -28,20 +34,20 @@ public class FrameAnimaux extends JFrame {
 	////////////////////////////////
 
 	private JPanel mainPanel;
-	
+
 	public JPanel getMainPanel() {
 		if (mainPanel == null) {
-		mainPanel = new JPanel();
-		
-		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-		mainPanel.add(getBoutonsPanel());
-//		mainPanel.add(getNomClientPanel());
-//		mainPanel.add(getInfosAnimalPanel());
-		
+			mainPanel = new JPanel();
+
+			mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+			mainPanel.add(getBoutonsPanel());
+			mainPanel.add(getNomClientPanel());
+			mainPanel.add(getInfosAnimalPanel());
+
 		}
 		return mainPanel;
 	}
-	
+
 	////////////////////////////////
 	// BOUTONS PANEL
 	////////////////////////////////
@@ -49,50 +55,247 @@ public class FrameAnimaux extends JFrame {
 	private JPanel boutonsPanel;
 	private JButton btnValider;
 	private JButton btnAnnuler;
-	
+
 	public JPanel getBoutonsPanel() {
-		if (boutonsPanel == null){
+		if (boutonsPanel == null) {
 			boutonsPanel = new JPanel();
 			boutonsPanel.setLayout(new FlowLayout());
-			Border border = BorderFactory.createLoweredBevelBorder();
+			Border border = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
 
 			boutonsPanel.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-			
+
 			boutonsPanel.setBorder(border);
-			boutonsPanel.add(getBtnValider());
 			boutonsPanel.add(getBtnAnnuler());
+			boutonsPanel.add(getBtnValider());
 		}
 		return boutonsPanel;
 	}
+
 	public JButton getBtnValider() {
-		if(btnValider == null){
+		if (btnValider == null) {
 			btnValider = new JButton("Valider");
-					//new ImageIcon(getClass().getResource("./resources/valider.png")));
+			// new
+			// ImageIcon(getClass().getResource("./resources/valider.png")));
 		}
 		return btnValider;
 	}
+
 	public JButton getBtnAnnuler() {
-		if(btnAnnuler == null){
+		if (btnAnnuler == null) {
 			btnAnnuler = new JButton("Annuler");
-					//new ImageIcon(getClass().getResource("./resources/annuler.png")));
+			// new
+			// ImageIcon(getClass().getResource("./resources/annuler.png")));
 		}
 		return btnAnnuler;
 	}
-	
-	
-	
-	
 
 	////////////////////////////////
 	// NOM CLIENT PANEL
 	////////////////////////////////
 
-	
+	private JPanel nomClientPanel;
+	private JTextField txtNomClient;
+
+	public JPanel getNomClientPanel() {
+		if (nomClientPanel == null) {
+			nomClientPanel = new JPanel();
+			TitledBorder border = new TitledBorder("Client :");
+			border.setTitleJustification(TitledBorder.LEFT);
+			border.setTitlePosition(TitledBorder.TOP);
+
+			nomClientPanel.setBorder(border);
+			nomClientPanel.add(getTxtNomClient());
+		}
+		return nomClientPanel;
+	}
+
+	public JTextField getTxtNomClient() {
+		if (txtNomClient == null) {
+			txtNomClient = new JTextField(30);
+		}
+		return txtNomClient;
+	}
 
 	////////////////////////////////
 	// INFOS ANIMAL PANEL
 	////////////////////////////////
 
+	// VARIABLES
+
+	private JPanel infosAnimalPanel;
+
+	private JLabel lblCode;
+	private JTextField txtCode;
+
+	private JLabel lblNomAnimal;
+	private JTextField txtNomAnimal;
+
+	private JComboBox<String> cbbSexeAnimal;
+
+	private JLabel lblCouleur;
+	private JTextField txtCouleurAnimal;
+
+	private JLabel lblEspeceAnimal;
+	private JComboBox<String> cbbEspeceAnimal;
+	private JLabel lblRaceAnimal;
+	private JComboBox<String> cbbRaceAnimal;
+
+	private JLabel lblTatouage;
+	private JTextField txtTatouage;
+
+	// GETTERS
+	public JPanel getInfosAnimalPanel() {
+		if(infosAnimalPanel == null){
+			infosAnimalPanel = new JPanel(new GridBagLayout());			
+			
+			addComponentTo(getLblCode(), infosAnimalPanel, 0, 0, 1, 1, 0.3, true);
+			addComponentTo(getTxtCode(), infosAnimalPanel, 1, 0, 1, 1, 0.7, true);
+			addComponentTo(getLblNomAnimal(), infosAnimalPanel, 0, 1, 1, 1, 0.3, true);
+			addComponentTo(getTxtNomAnimal(), infosAnimalPanel, 1, 1, 1, 1, 0.7, true);
+			addComponentTo(getCbbSexeAnimal(), infosAnimalPanel, 3, 1, 1, 1, 1, true);
+			addComponentTo(getLblCouleur(), infosAnimalPanel, 0, 2, 1, 1, 1, true);
+			addComponentTo(getTxtCouleurAnimal(), infosAnimalPanel, 1, 2, 1, 1, 1, true);
+			addComponentTo(getLblEspeceAnimal(), infosAnimalPanel, 0, 3, 1, 1, 1, true);
+			addComponentTo(getCbbEspeceAnimal(), infosAnimalPanel, 1, 3, 1, 1, 1, true);
+			addComponentTo(getLblRaceAnimal(), infosAnimalPanel, 2, 3, 1, 1, 1, true );
+			addComponentTo(getCbbRaceAnimal(), infosAnimalPanel, 3, 3, 1, 1, 1, true);
+			addComponentTo(getLblTatouage(), infosAnimalPanel, 0, 4, 1, 1, 1, true);
+			addComponentTo(getTxtTatouage(), infosAnimalPanel, 1, 4, 1, 1, 1, true);
+					
+			
+		}
+		return infosAnimalPanel;
+	}
+
+	public JLabel getLblCode() {
+		if (lblCode == null) {
+			lblCode = new JLabel("Code");
+		}
+		return lblCode;
+	}
+
+	public JTextField getTxtCode() {
+		if (txtCode == null) {
+			txtCode = new JTextField(30);
+		}
+		return txtCode;
+	}
+
+	public JLabel getLblNomAnimal() {
+		if (lblNomAnimal == null) {
+			lblNomAnimal = new JLabel("Nom");
+		}
+		return lblNomAnimal;
+	}
+
+	public JTextField getTxtNomAnimal() {
+		if (txtNomAnimal == null) {
+			txtNomAnimal = new JTextField(30);
+		}
+		return txtNomAnimal;
+	}
+	
+	
+	public JComboBox<String> getCbbSexeAnimal() {
+		if (cbbSexeAnimal == null) {
+			cbbSexeAnimal = new JComboBox<>();
+		}
+		return cbbSexeAnimal;
+	}
+
+	public JLabel getLblCouleur() {
+		if (lblCouleur == null) {
+			lblCouleur = new JLabel("Couleur");
+		}
+		return lblCouleur;
+
+	}
+
+	public JTextField getTxtCouleurAnimal() {
+		if (txtCouleurAnimal == null) {
+			txtCouleurAnimal = new JTextField(30);
+		}
+		return txtCouleurAnimal;
+	}
+
+	public JLabel getLblEspeceAnimal() {
+		if (lblEspeceAnimal == null) {
+			lblEspeceAnimal = new JLabel("Espèce");
+		}
+		return lblEspeceAnimal;
+	}
+
+	public JComboBox<String> getCbbEspeceAnimal() {
+		if (cbbEspeceAnimal == null) {
+			cbbEspeceAnimal = new JComboBox<>();
+		}
+		return cbbEspeceAnimal;
+	}
+
+	public JLabel getLblRaceAnimal() {
+		if (lblRaceAnimal == null) {
+			lblRaceAnimal = new JLabel("Race");
+		}
+		return lblRaceAnimal;
+	}
+
+	public JComboBox<String> getCbbRaceAnimal() {
+		if (cbbRaceAnimal == null) {
+			cbbRaceAnimal = new JComboBox<>();
+		}
+		return cbbRaceAnimal;
+	}
+
+	public JLabel getLblTatouage() {
+		if (lblTatouage == null) {
+			lblTatouage = new JLabel("Tatouage");
+		}
+		return lblTatouage;
+	}
+
+	public JTextField getTxtTatouage() {
+		if (txtTatouage == null) {
+			txtTatouage = new JTextField(30);
+		}
+		return txtTatouage;
+	}
+
+	///////////////////////////////////
+	// METHODES
+	///////////////////////////////////
+
+	private void addComponentTo(JComponent component, JPanel panel, int x, int y, int width, int height, double weightX,
+			boolean fillHorizontal) {
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = x;
+		gbc.gridy = y;
+		gbc.gridwidth = width;
+		gbc.gridheight = height;
+		gbc.weightx = weightX;
+		if (fillHorizontal) {
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+		}
+		gbc.insets = new Insets(7, 10, 5, 10);
+		panel.add(component, gbc);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
