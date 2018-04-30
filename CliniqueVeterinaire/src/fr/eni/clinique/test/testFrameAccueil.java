c29ab8419e55417356843f510f9d7760cd12ab7c
cmenerville2018 <cmenerville2018@41-UC44-16.ad.campus-eni.fr>
2018-04-30 11:16:33 +0200

    classes de test séparée

---
+++ CliniqueVeterinaire/src/fr/eni/clinique/test/testFrameAccueil.java
@@ -0,0 +1,28 @@
+package fr.eni.clinique.test;
+
+import javax.swing.SwingUtilities;
+
+import fr.eni.clinique.ihm.FrameAnimaux;
+import fr.eni.clinique.ihm.FrameClients;
+import fr.eni.clinique.ihm.FrameConnexion;
+import fr.eni.clinique.ihm.FramePriseRDV;
+
+public class testFrameAccueil {
+
+	public static void main(String[] args) {
+
+
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				
+			}
+		});
+
+		
+		
+	}
+
+}
--- CliniqueVeterinaire/src/fr/eni/clinique/test/testFrameAnimaux.java
+++ CliniqueVeterinaire/src/fr/eni/clinique/test/testFrameAnimaux.java
@@ -7,7 +7,7 @@ import fr.eni.clinique.ihm.FrameClients;
 import fr.eni.clinique.ihm.FrameConnexion;
 import fr.eni.clinique.ihm.FramePriseRDV;
 
-public class testIHM {
+public class testFrameAnimaux {
 
 	public static void main(String[] args) {
 
@@ -16,20 +16,10 @@ public class testIHM {
 			
 			@Override
 			public void run() {
-				FrameAnimaux frame = new FrameAnimaux();
-				frame.setVisible(false);
 				
-				FramePriseRDV rdvFrame = new FramePriseRDV();
-				rdvFrame.setVisible(false);
-				
-				FrameConnexion connexionFrame = new FrameConnexion();
-				connexionFrame.setVisible(false);
 				
 				FrameAnimaux animauxFrame = new FrameAnimaux();
 				animauxFrame.setVisible(true);
-				
-				FrameClients clientsFrame = new FrameClients();
-				clientsFrame.setVisible(false);
 				
 				
 				
